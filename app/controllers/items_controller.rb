79e7d47de022e698a0c1d4fb4a88cfc07c41f4a1
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-11-26 10:55:37 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :item_not_found
+
+  def show
+    items = Item.find(params[:id])
+    render json: items
+  end
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items 
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create 
+    user = User.find(params[:user_id])
+    user.items << Item.create(item_params)
+    render json: user.items.last, status: :created
+
+  end
+
+  private
+
+  def item_not_found
+     render json: { error: 'Item not found'}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name,:description,:price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # localhost:3000/users/i
+  resources :users, only: [:show] do
+    # localhost:3000/users/1/items/1
+    resources :items, only: [:index, :show, :create]
+  end
+  
   resources :items, only: [:index]
-  resources :users, only: [:show]
 end
